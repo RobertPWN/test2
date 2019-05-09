2b50c42ff78d72939501bc18b8b0c046e0dee00c
Pawel <pawel33@gmail.com>
2019-05-09 08:06:39 +0200

    tre

---
+++ test.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test {
+//dsadsadaeeeeeeea
+    public static void main(String[] args) {
+
+
+        for(int i=0; i<100; i++){
+
+            if (i % 2 == 0){
+                continue;
+            }
+            //dasdasdasdasda
+            System.out.println(i);
+
+        }
+    }
+}
